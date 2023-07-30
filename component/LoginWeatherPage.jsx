7cfc1b349a6aed0654884377cfa1759542552f1d
Rajshree Jajoo <rajshreejajoo77@gmail.com>
2023-07-31 00:27:24 +0530

    align changes

--- component/LoginWeatherPage.jsx
+++ component/LoginWeatherPage.jsx
@@ -25,15 +25,15 @@ const LoginWeatherPage = ({ navigation }) => {
                     uri: 'https://img.freepik.com/free-vector/blue-cloudy-daylight-background-weather-design_33099-512.jpg'
                 }}>
                 <View style={styles.bg}>
-                    <Text> !! Login Weather Page !!</Text>
+                    <Text> !! Weather Page !!</Text>
                     <View style={{ flexDirection: 'row', marginTop: 50 }}>
                         <Text style={{ marginRight: 100 }}>{date}</Text>
                         <Text style={{ marginLeft: 100 }}>{time}</Text>
                     </View>
 
                     <Image source={{ uri: 'https://newsonair.com/wp-content/uploads/2021/06/WEATHER-5.jpg' }} style={{ width: 400, height: 200, marginTop: 200 }} />
-                    <View style={{ marginTop: 50 }}>
-                        <Button title='Next' onPress={() => navigation.navigate('Name')} />
+                    <View style={{ marginTop: 10}}>
+                        <Button color='black' title='Next' onPress={() => navigation.navigate('Name')} />
                     </View>
 
                 </View>
